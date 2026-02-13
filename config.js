7dbd7bba37fa7975126a5b1f79680f0d2d3a9ba5
HuiXin-01 <83772923+HuiXin-01@users.noreply.github.com>
2026-02-13 21:39:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ const CONFIG = {
         backgroundEnd: "#ABE7B2",        // Gradient end (should complement backgroundStart)
         buttonBackground: "#000000",     // Button color (should stand out against the background)
         buttonHover: "#ECF4E8",          // Button hover color (slightly lighter than buttonBackground)
-        textColor: "#ff4757"             // Text color (make sure it's readable!)
+        textColor: "#1C4D8D"             // Text color (make sure it's readable!)
     },
 
     // Animation settings
